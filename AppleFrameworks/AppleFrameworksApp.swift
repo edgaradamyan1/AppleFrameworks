b7fb87891d4f87c724219852a6afb6847683d84c
Edgar Adamyan <edadamyan1@gmail.com>
2025-03-13 21:09:48 +0100

    Initial Commit

---
+++ AppleFrameworks/AppleFrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleFrameworksApp.swift
+//  AppleFrameworks
+//
+//  Created by Edgar Adamyan on 13.03.25.
+//
+
+import SwiftUI
+
+@main
+struct AppleFrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
